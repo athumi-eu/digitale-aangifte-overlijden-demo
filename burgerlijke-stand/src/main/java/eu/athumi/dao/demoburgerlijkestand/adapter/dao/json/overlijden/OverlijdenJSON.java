21ef3e7ec3182e642b52fc2fe2da0867595ed79f
Joris Lefever <jorislefever@hotmail.com>
2024-09-03 09:15:26 +0200

    #DAO-71: Voeg deduction based deserialisation toe voor Plaats

--- burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/overlijden/OverlijdenJSON.java
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/overlijden/OverlijdenJSON.java
@@ -1,14 +1,13 @@
 package eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.overlijden;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.TijdstipJSON;
 import eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.Type;
 import eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.plaats.Plaats;
 
 import java.util.List;
 
-public record OverlijdenJSON(TijdstipJSON tijdstip, @JsonIgnore List<Plaats> plaats, boolean doodgeboorte)
+public record OverlijdenJSON(TijdstipJSON tijdstip, List<Plaats> plaats, boolean doodgeboorte)
     implements Type {
     @Override
     public String type() {
