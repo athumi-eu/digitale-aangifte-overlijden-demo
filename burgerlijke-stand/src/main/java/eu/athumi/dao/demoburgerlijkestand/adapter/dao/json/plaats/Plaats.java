8286d567db90bc7abb762e9d03e4aee52f48065b
Joris Lefever <jorislefever@hotmail.com>
2024-09-03 09:15:26 +0200

    #DAO-71: Voeg deduction based deserialisation toe voor Plaats

--- burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/plaats/Plaats.java
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/plaats/Plaats.java
@@ -1,7 +1,11 @@
 package eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.plaats;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION)
+@JsonSubTypes({ @JsonSubTypes.Type(AdresJSON.class), @JsonSubTypes.Type(LocatieJSON.class) })
 public interface Plaats {
     @JsonProperty("@type")
     default String type() {
