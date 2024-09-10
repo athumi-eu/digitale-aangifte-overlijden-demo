de1daa65fce52957ace44471f11780af0354790a
Joris Lefever <jorislefever@hotmail.com>
2024-09-10 16:36:21 +0200

    #DAO-101: Toon gekoppelde verslagen op het dossier detail

--- burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/DossierDao.java
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/DossierDao.java
@@ -2,8 +2,10 @@ package eu.athumi.dao.demoburgerlijkestand.adapter.dao;
 
 import eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.DossierBurgerlijkeStandJSON;
 import eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.VaststellingType;
+import eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.verslag.VerslagBeedigdArtsJSON;
 import eu.athumi.dao.demoburgerlijkestand.adapter.dao.parsing.JongerDanEenJaarParser;
 import eu.athumi.dao.demoburgerlijkestand.adapter.dao.parsing.OuderDanEenJaarParser;
+import eu.athumi.dao.demoburgerlijkestand.adapter.dao.parsing.VerslagParser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestClient;
 
+import java.net.URI;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.Optional;
@@ -34,10 +37,10 @@ public class DossierDao {
     @GetMapping(value = "/dossiers")
     public String dossier(Model model, @RequestParam String kbonummer) {
         DossierBurgerlijkeStandJSON[] response = securedWebClient
-            .get()
-            .uri(daoServiceUrl + "/burgerlijke-stand/dossiers?kbonummer={kbonummer}", kbonummer)
-            .retrieve()
-            .body(DossierBurgerlijkeStandJSON[].class);
+                .get()
+                .uri(daoServiceUrl + "/burgerlijke-stand/dossiers?kbonummer={kbonummer}", kbonummer)
+                .retrieve()
+                .body(DossierBurgerlijkeStandJSON[].class);
 
         model.addAttribute("dossiers", response);
         model.addAttribute("kbonummer", kbonummer);
@@ -48,25 +51,26 @@ public class DossierDao {
     @GetMapping(value = "/dossier")
     public String dossierDetail(Model model, @RequestParam String id, @RequestParam String kbonummer) {
         Optional<DossierBurgerlijkeStandJSON> detail = Arrays.stream(securedWebClient
-                .get()
-                .uri(daoServiceUrl + "/burgerlijke-stand/dossiers?kbonummer={kbonummer}", kbonummer)
-                .retrieve()
-                .body(DossierBurgerlijkeStandJSON[].class))
-            .filter(dossier -> Objects.equals(dossier.id(), id))
-            .findFirst();
+                        .get()
+                        .uri(daoServiceUrl + "/burgerlijke-stand/dossiers?kbonummer={kbonummer}", kbonummer)
+                        .retrieve()
+                        .body(DossierBurgerlijkeStandJSON[].class))
+                .filter(dossier -> Objects.equals(dossier.id(), id))
+                .findFirst();
 
         model.addAttribute("kbonummer", kbonummer);
 
         if (detail.isPresent()) {
             var dossier = detail.get();
+            var verslag = Optional.ofNullable(dossier.verslagDetailURL()).map(this::getVerslagDetail).map(VerslagParser::new).orElse(null);
             if (Objects.equals(VaststellingType.OVERLIJDEN_PERSOON_OUDER_DAN_1_JAAR, dossier.vaststellingType())) {
                 model.addAttribute("dossier", dossier);
-                model.addAttribute("verslag", null);
+                model.addAttribute("verslag", verslag);
                 model.addAttribute("parsedDetail", new OuderDanEenJaarParser(dossier));
                 return "detail-ouder-dan-1-jaar";
             } else {
                 model.addAttribute("dossier", dossier);
-                model.addAttribute("verslag", null);
+                model.addAttribute("verslag", verslag);
                 model.addAttribute("parsedDetail", new JongerDanEenJaarParser(dossier));
                 return "detail-jonger-dan-1-jaar";
             }
@@ -74,5 +78,16 @@ public class DossierDao {
         }
         return "detail-does-not-exist";
     }
+
+    public VerslagBeedigdArtsJSON getVerslagDetail(URI verslagDetailURL) {
+
+        VerslagBeedigdArtsJSON body = securedWebClient
+                .get()
+                .uri(verslagDetailURL)
+                .retrieve()
+                .body(VerslagBeedigdArtsJSON.class);
+        return body;
+    }
+
 
 }
