e23cfdddca15db734c0a50de2ac09ed0cf2ae3db
vercruyssetim <vercruysse.tim@gmail.com>
2024-09-11 15:09:56 +0200

    #DAO-77 uploaden van aktes

---
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/FileDao.java
@@ -0,0 +1,38 @@
+package eu.athumi.dao.demoburgerlijkestand.adapter.dao;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Controller
+public class FileDao {
+
+    private final RestClient securedWebClient;
+    private String daoServiceUrl;
+
+    public FileDao(RestClient securedWebClient, @Value("${dao.service.connection-url}") String daoServiceUrl) {
+        this.securedWebClient = securedWebClient;
+        this.daoServiceUrl = daoServiceUrl;
+    }
+
+    @PostMapping("/files/upload")
+    public String uploadFile(@RequestParam("akte") MultipartFile file, @RequestParam String type, @RequestParam String id) throws IOException {
+        MultiValueMap<String, Resource> body = new LinkedMultiValueMap<>();
+        body.add("akte", file.getResource());
+        securedWebClient
+                .post()
+                .uri(daoServiceUrl + "/burgerlijke-stand/dossiers/{dossierId}/aktes/{type}", id, type )
+                .body(body)
+                .retrieve()
+                .toBodilessEntity();
+        return "dossiers";
+    }
+}
--- burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/configuration/DaoRestTemplate.java
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/configuration/DaoRestTemplate.java
@@ -2,6 +2,7 @@ package eu.athumi.dao.demoburgerlijkestand.adapter.dao.configuration;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
