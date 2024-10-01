6db92ff152a26471a976177c33fbc680c3ab4e72
Joris Lefever <jorislefever@hotmail.com>
2024-10-01 15:35:18 +0200

    #DAO-113: Voeg Checkboxes toe

--- burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/verrijking/VaderOfMeeMoederJSON.java
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/verrijking/VaderOfMeeMoederJSON.java
@@ -1,3 +1,3 @@
 package eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.verrijking;
 
-public record VaderOfMeeMoederJSON(String rijksregisternummer, Boolean nietVantoepassing) {}
+public record VaderOfMeeMoederJSON(String rijksregisternummer, boolean nietVantoepassing) {}
