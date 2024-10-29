59c49cb5aee0075f02cc076c987fcd54bae8600c
tom-o-thy <tom.aerts@kunlabora.be>
2024-10-29 13:46:03 +0100

    DAO-117: refactor UUID crematoriumInBelgie naar KBONummer/String

--- burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/inlichtingenfiche/CrematieJSON.java
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/inlichtingenfiche/CrematieJSON.java
@@ -1,9 +1,7 @@
 package eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.inlichtingenfiche;
 
-import java.util.UUID;
-
 public record CrematieJSON(
-    UUID crematoriumInBelgie,
+    String crematoriumInBelgie,
     String anderCrematorium,
     BestemmingAsJSON bestemmingAs,
     AsWettelijkePartnerJSON asWettelijkePartner
