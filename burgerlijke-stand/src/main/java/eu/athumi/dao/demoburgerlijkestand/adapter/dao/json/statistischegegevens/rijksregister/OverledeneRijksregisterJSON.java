2d7772f9b287c7420ff6e930678b62dc0f62f62e
vercruyssetim <vercruysse.tim@gmail.com>
2024-12-10 17:36:58 +0100

    #DAO-136 parsing burgerlijkestaat and nationaliteit

--- burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/statistischegegevens/rijksregister/OverledeneRijksregisterJSON.java
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/statistischegegevens/rijksregister/OverledeneRijksregisterJSON.java
@@ -1,13 +1,18 @@
 package eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.statistischegegevens.rijksregister;
 
 import eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.Geslacht;
+import eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.statistischegegevens.NationaliteitJSON;
 import eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.statistischegegevens.locatie.AdresJSON;
+import eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.statistischegegevens.overledene.BurgerlijkeStaatJSON;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public record OverledeneRijksregisterJSON(
         Geslacht geslacht,
         LocalDate geboortedatum,
-        AdresJSON verblijfsAdres
+        AdresJSON verblijfsAdres,
+        NationaliteitJSON nationaliteit,
+        List<BurgerlijkeStaatJSON> burgerlijkeStaten
 ) {
 }
