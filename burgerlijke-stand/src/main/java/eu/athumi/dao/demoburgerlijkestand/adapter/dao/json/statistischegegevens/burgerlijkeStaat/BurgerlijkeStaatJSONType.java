e31fc1a2ffb24052685aaa8d58f22e85b9161120
gielese <gielenseppe@gmail.com>
2024-12-24 12:04:31 +0100

    Bug Burgelijke staat

--- burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/statistischegegevens/burgerlijkeStaat/BurgerlijkeStaatJSONType.java
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/statistischegegevens/burgerlijkeStaat/BurgerlijkeStaatJSONType.java
@@ -2,4 +2,9 @@ package eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.statistischegegevens
 
 public enum BurgerlijkeStaatJSONType {
     GEHUWD,
+    ONGEHUWD,
+    WEDUWE,
+    UIT_ECHT_GESCHEIDEN,
+    VAN_TAFEL_EN_BED_GESCHEIDEN,
+    ONBEKEND,
 }
