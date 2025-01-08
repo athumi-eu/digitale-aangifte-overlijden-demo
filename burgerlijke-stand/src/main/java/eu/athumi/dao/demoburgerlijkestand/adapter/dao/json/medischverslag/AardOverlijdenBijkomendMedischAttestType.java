220aeaffb15ae66a67d4048160a084a41f075e35
Joris Lefever <jorislefever@hotmail.com>
2025-01-08 15:36:08 +0100

    #DAO-126: Voeg Bijkomend medisch attest toe

---
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/medischverslag/AardOverlijdenBijkomendMedischAttestType.java
@@ -0,0 +1,7 @@
+package eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.medischverslag;
+
+public enum AardOverlijdenBijkomendMedischAttestType {
+    NatuurlijkeOorzaak,
+    GewelddadigeOfVerdachteOorzaak,
+    NietVastTeStellenOorzaak,
+}
--- burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/medischverslag/MedischVerslag.java
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/medischverslag/MedischVerslag.java
@@ -1,7 +1,27 @@
 package eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.medischverslag;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.verslag.MedischVerslagBeedigdArtsJSON;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "@type")
+@JsonSubTypes(
+        {
+                @JsonSubTypes.Type(
+                        value = BijkomendMedischAttestJSON.class,
+                        name = "BijkomendMedischAttest"
+                ),
+                @JsonSubTypes.Type(
+                        value = MedischVerslagBeedigdArtsJSON.class,
+                        name = "VerslagBeedigdArts"
+                ),
+                @JsonSubTypes.Type(
+                        value = VaststellingOverlijdenJSON.class,
+                        name = "MedischVerslagOverlijden"
+                ),
+        }
+)
 public interface MedischVerslag {
     @JsonProperty("@type")
     default String type() {
