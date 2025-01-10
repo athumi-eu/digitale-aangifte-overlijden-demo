1482495ceb9eddef8a2155744eef704b49d0c96e
tom-o-thy <tom.aerts@kunlabora.be>
2025-01-10 16:44:22 +0100

    DAO-183: Land van geboorte

---
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/plaats/GemeenteEnLand.java
@@ -0,0 +1,3 @@
+package eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.plaats;
+
+public record GemeenteEnLand(Gemeente gemeente, Land land, String buitenlandseGemeente) {}
---
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/plaats/Land.java
@@ -0,0 +1,3 @@
+package eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.plaats;
+
+public record Land(String niscode, String naam) {}
