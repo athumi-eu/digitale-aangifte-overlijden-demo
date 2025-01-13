c9c7fd5a2532c34b40e63a780100cc9e9a0588e0
tom-o-thy <tom.aerts@kunlabora.be>
2025-01-13 15:21:08 +0100

    DAO-183: geboorte uit RR

--- burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/plaats/GemeenteEnLand.java
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/plaats/GemeenteEnLand.java
@@ -1,3 +1,32 @@
 package eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.plaats;
 
-public record GemeenteEnLand(Gemeente gemeente, Land land, String buitenlandseGemeente) {}
+import java.util.Optional;
+
+public record GemeenteEnLand(Gemeente gemeente, Land land, String buitenlandseGemeente) {
+    @Override
+    public String toString() {
+        var gemeenteString = "";
+        var landString = "";
+        if(this.gemeente() != null) {
+            if(this.gemeente().naam() != null) {
+                gemeenteString += this.gemeente().naam();
+            }
+            if(this.gemeente().niscode() != null) {
+                gemeenteString += " (" + this.gemeente().niscode() + ")";
+            }
+        }
+
+        if(this.land() != null) {
+            if(this.land().naam() != null) {
+                landString += this.land().naam();
+            }
+            if(this.land().niscode() != null) {
+                landString += " (" + this.land().niscode() + ")";
+            }
+        }
+
+        var buitenlandString = Optional.ofNullable(this.buitenlandseGemeente()).orElse("");
+
+        return String.join(" ", gemeenteString, landString, buitenlandString);
+    }
+}
--- burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/statistischegegevens/vaststelling/MoederVaststellingJSON.java
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/statistischegegevens/vaststelling/MoederVaststellingJSON.java
@@ -1,6 +1,8 @@
 package eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.statistischegegevens.vaststelling;
 
 import eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.Geslacht;
+import eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.plaats.GemeenteEnLand;
 import eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.statistischegegevens.overlijdensgegevens.AdresStatistischJSON;
 
-public record MoederVaststellingJSON(Geslacht geslacht, AdresStatistischJSON verblijfplaats) {}
+public record MoederVaststellingJSON(Geslacht geslacht, AdresStatistischJSON verblijfplaats,
+                                     GemeenteEnLand geboorteAdres) {}
