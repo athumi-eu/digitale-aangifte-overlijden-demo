4efc3fe6358063c35a32fd9790eabfdf73ac23d3
vercruyssetim <vercruysse.tim@gmail.com>
2025-02-12 11:51:29 +0100

    #DAO-275 doodgeboorte < 180 dagen toegevoegd

--- burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/DossierStatus.java
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/DossierStatus.java
@@ -3,7 +3,7 @@ package eu.athumi.dao.demoburgerlijkestand.adapter.dao.json;
 public enum DossierStatus {
     NIEUW,
     IN_BEHANDELING,
-    NIET_BEHANDELD,
     BEHANDELD,
-    VERWIJDERD
+    VERWIJDERD,
+    ZWANGERSCHAP_MINDER_DAN_180D
 }
