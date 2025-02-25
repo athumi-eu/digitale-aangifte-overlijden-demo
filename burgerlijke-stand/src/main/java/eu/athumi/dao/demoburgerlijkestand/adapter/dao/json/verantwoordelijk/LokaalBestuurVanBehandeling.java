4a8d18d124a0a6c1b1acd26e7a95604cd6dc7db6
vercruyssetim <vercruysse.tim@gmail.com>
2025-02-25 11:33:42 +0100

    #DAO-275 added lokaal bestuur van behandeling

--- burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/verantwoordelijk/LokaalBestuurVanBehandeling.java
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/verantwoordelijk/LokaalBestuurVanBehandeling.java
@@ -1,4 +1,4 @@
 package eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.verantwoordelijk;
 
-public record LokaalBestuurVanBehandeling(String kboNummer) {
+public record LokaalBestuurVanBehandeling(String kboNummer, String naam, String niscode, String postcode) {
 }
