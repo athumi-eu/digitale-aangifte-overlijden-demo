8734104fd144ddadbda06b04e08040d6544bf41e
vercruyssetim <vercruysse.tim@gmail.com>
2025-04-01 14:04:38 +0200

    #DAO-328: refactoren asWettelijkePartner

--- burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/inlichtingenfiche/CrematieJSON.java
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/inlichtingenfiche/CrematieJSON.java
@@ -3,6 +3,5 @@ package eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.inlichtingenfiche;
 public record CrematieJSON(
     String crematoriumInBelgie,
     String anderCrematorium,
-    BestemmingAsJSON bestemmingAs,
-    AsWettelijkePartnerJSON asWettelijkePartner
+    BestemmingAsJSON bestemmingAs
 ) {}
