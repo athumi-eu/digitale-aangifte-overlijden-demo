6c44e5d428dce85972f513f63b6face7c5a9e0b5
gielese <gielenseppe@gmail.com>
2025-04-02 10:11:19 +0200

    Demo app ook gebruik maken van zonedDateTime

--- crematorium/tests/aanroepen.toestemmingen.spec.ts
+++ crematorium/tests/aanroepen.toestemmingen.spec.ts
@@ -4,7 +4,7 @@ import {describe, expect, test} from "vitest";
 describe('oproepen van toestemmingen api', () => {
     test('Oproepen met enkel van ', async () => {
         const axios = getAxios()
-        const response = await axios.get('/crematorium/v1/toestemmingen?van=2024-12-20T10:00:00')
+        const response = await axios.get('/crematorium/v1/toestemmingen?van=2024-12-20T10:00:00+02:00')
 
         console.log(response.data);
         expect(response.status).toEqual(200);
@@ -12,7 +12,7 @@ describe('oproepen van toestemmingen api', () => {
 
     test('Oproepen met van en tot', async () => {
         const axios = getAxios()
-        const response = await axios.get('/crematorium/v1/toestemmingen?van=2024-12-20T10:00:00&tot=2024-12-28T10:00:00')
+        const response = await axios.get('/crematorium/v1/toestemmingen?van=2024-12-20T10:00+02:00:00&tot=2024-12-28T10:00:00+02:00')
 
         console.log(response.data);
         expect(response.status).toEqual(200);
