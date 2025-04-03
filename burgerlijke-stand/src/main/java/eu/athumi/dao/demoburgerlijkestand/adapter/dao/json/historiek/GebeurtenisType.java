1709a6462d61071970df07d5e1d165fb51206cd0
vercruyssetim <vercruysse.tim@gmail.com>
2025-04-03 11:14:22 +0200

    #DAO-311: gebeurtenis types toegevoegd, reden overwijderen toegevoegd

--- burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/historiek/GebeurtenisType.java
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/historiek/GebeurtenisType.java
@@ -3,5 +3,6 @@ package eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.historiek;
 public enum GebeurtenisType {
     UITVAART_ONTKOPPELD,
     DOSSIER_HEROPEND,
-    LOKAAL_BESTUUR_VAN_BEHANDELING_GEWIJZIGD
+    LOKAAL_BESTUUR_VAN_BEHANDELING_GEWIJZIGD,
+    AFGESLOTEN, OPGESTART, DOSSIER_VERWIJDERD
 }
