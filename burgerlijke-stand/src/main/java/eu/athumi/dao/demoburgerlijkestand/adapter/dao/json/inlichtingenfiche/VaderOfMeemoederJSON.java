56dad77ad9632e7c59d2ec37eb0552060a523a43
gielese <gielenseppe@gmail.com>
2025-10-23 14:46:20 +0200

    Issue parsedGeboorteDatum

--- burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/inlichtingenfiche/VaderOfMeemoederJSON.java
+++ burgerlijke-stand/src/main/java/eu/athumi/dao/demoburgerlijkestand/adapter/dao/json/inlichtingenfiche/VaderOfMeemoederJSON.java
@@ -1,6 +1,9 @@
 package eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.inlichtingenfiche;
 
 import eu.athumi.dao.demoburgerlijkestand.adapter.dao.json.Geslacht;
+import eu.athumi.dao.demoburgerlijkestand.adapter.dao.parsing.TijdstipParser;
+
+import java.util.Optional;
 
 public record VaderOfMeemoederJSON(
         String rijksregisternummer,
@@ -10,4 +13,7 @@ public record VaderOfMeemoederJSON(
         String geboortelocatie,
         String geboorteDatum
 ) {
+    public String parsedGeboorteDatum() {
+        return Optional.ofNullable(geboorteDatum).map(TijdstipParser::formatDateString).orElse("");
+    }
 }
